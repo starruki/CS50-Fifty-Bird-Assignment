0b441557b6446d632dd95359245e798e9c957fdd
unknown <lisaliiiiu2002@gmail.com>
2020-07-23 13:35:43 -0500

    first commit

---
+++ states/TitleScreenState.lua
@@ -0,0 +1,32 @@
+--[[
+    TitleScreenState Class
+    
+    Author: Colton Ogden
+    cogden@cs50.harvard.edu
+
+    The TitleScreenState is the starting screen of the game, shown on startup. It should
+    display "Press Enter" and also our highest score.
+]]
+
+TitleScreenState = Class{__includes = BaseState}
+
+function TitleScreenState:init()
+    -- nothing
+end
+
+function TitleScreenState:update(dt)
+    if love.keyboard.wasPressed('enter') or love.keyboard.wasPressed('return') then
+        gStateMachine:change('countdown')
+    end
+end
+
+function TitleScreenState:render()
+    love.graphics.setFont(flappyFont)
+    love.graphics.printf('Fifty Bird', 0, 64, VIRTUAL_WIDTH, 'center')
+
+    love.graphics.setFont(mediumFont)
+    love.graphics.printf('Press Enter to play', 0, 100, VIRTUAL_WIDTH, 'center')
+
+    love.graphics.setFont(smallFont)
+    love.graphics.printf('Press p to pause while in game', 0, 130, VIRTUAL_WIDTH, 'center')
+end
